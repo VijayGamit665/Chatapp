685ab21f4728f11780353eb47237c9d7b7fce19c
VijayGamit665 <vijaygamit665@gmail.com>
2025-01-11 16:05:33 +0530

    Web Socket Configuration and create chat controller

--- chat-app-backend/chat-app-backend/src/main/java/com/substring/chat/entities/Message.java
+++ chat-app-backend/chat-app-backend/src/main/java/com/substring/chat/entities/Message.java
@@ -17,9 +17,27 @@ public class Message {
     private String content;
     private LocalDateTime timeStamp;
 
-    public Message(String sender, String content) {
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
         this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
         this.content = content;
-        this.timeStamp = LocalDateTime.now();
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
     }
 }
